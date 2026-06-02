fa222fff8fff26bd2c84a208215a0e7d24f60ad6
Vishal Vivek <vishal.vivek.10039@gmail.com>
2026-06-02 17:50:28 +0530

    Progressing to control and if statments

--- basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -7,4 +7,24 @@ function add() {
   outputResult(currentResult, '');
 }
 
+function subtract() {
+  currentResult = currentResult - parseInt(userInput.value);
+  outputResult(currentResult, '');
+}
+
+function multiply() {
+  currentResult = currentResult * parseInt(userInput.value);
+  outputResult(currentResult, '');
+}
+
+function divide() {
+  currentResult = currentResult / parseInt(userInput.value);
+  outputResult(currentResult, '');
+}
+
+
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
